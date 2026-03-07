52e5e51d0dab55dfb631c2e61ec2685d7cc0bc56
Owolabi Fawemimo <f.owolabi81@gmail.com>
2026-03-07 18:53:47 +0100

    MySql started

--- training/src/main/java/com/TrainingApplication.java
+++ training/src/main/java/com/TrainingApplication.java
@@ -15,22 +15,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class TrainingApplication {
 
 	 static void main(String[] args) {
-
-		ConfigurableApplicationContext context = SpringApplication.run(TrainingApplication.class, args);
-//		var order = context.getBean(OrderService.class);
-//		var order = new OrderService(new PayPalService());
-//		order.placeOrder();
-
-//		var notifyUser = context.getBean(NotificationManager.class);
-//		notifyUser.sendNotification("Email sent to the user");
-//
-//		var heavyResource = context.getBean(HeavyResource.class);
-//
-//		context.close();
-		 var user = new User("BigDam@gmail.com","passW@7d", "BigDam");
-		 System.out.println("User service is been called");
-		 var regUser = context.getBean(UserService.class);
-		 regUser.registerUser(user);
+		 SpringApplication.run(TrainingApplication.class, args);
 	 }
 
 }
--- training/src/main/java/com/excerise3/EmailNotificationProviderService.java
+++ training/src/main/java/com/excerise3/EmailNotificationProviderService.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
-@Service("email")
+@Service()
 @Primary
 public class EmailNotificationProviderService implements NotificationInterface {
 
